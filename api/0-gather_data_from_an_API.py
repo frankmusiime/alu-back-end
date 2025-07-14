e94dab9e6240dffc2888ab087bb2f54d074ab23a
frankmusiime <f.musiime@alustudent.com>
2025-07-14 10:53:26 -0700

    my api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -27,12 +27,8 @@ def fetch_employee_todo_progress(employee_id):
     Args:
         employee_id (int): The ID of the employee.
     """
-    user_url = (
-        f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    )
-    todos_url = (
-        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    )
+    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
 
     user_response = requests.get(user_url)
     if user_response.status_code != 200:
@@ -48,12 +44,14 @@ def fetch_employee_todo_progress(employee_id):
     done_tasks = [task for task in todos if task.get("completed")]
     number_of_done_tasks = len(done_tasks)
 
-    print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+    print(
+        f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):"
+    )
     for task in done_tasks:
-        print(f"\t {task.get('title')}")
+        print(f"\t{task.get('title')}")
 
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) != 2:
         print("Usage: ./todo.py <employee_id>")
         sys.exit(1)
@@ -63,4 +61,3 @@ if __name__ == "__main__":
     except ValueError:
         print("Employee ID must be an integer.")
         sys.exit(1)
-    fetch_employee_todo_progress(emp_id)
