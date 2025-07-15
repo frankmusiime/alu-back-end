3b8a406d38b1640c4f790397664bb7cf35c0e844
frankmusiime <f.musiime@alustudent.com>
2025-07-15 06:47:21 -0700

    my api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,69 +1,35 @@
 #!/usr/bin/python3
 """
-This script uses a REST API to retrieve and display the TODO list progress
-of an employee based on their ID.
-
-Usage:
-    ./todo.py <employee_id>
-
-Requirements:
-    - requests
-
-PEP8 Validation:
-    - Imports are ordered alphabetically
-    - Line length <= 79 characters
-    - Functions and logic are properly spaced and indented
-    - Docstrings follow PEP257 convention
+getting data using api
 """
-
 import requests
 import sys
 
-
-def fetch_employee_todo_progress(employee_id):
-    """
-    Fetches and displays the TODO list progress for a given employee ID.
-
-    Args:
-        employee_id (int): The ID of the employee.
-    """
-    user_url = (
-        f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    )
-    todos_url = (
-        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    )
-
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        print(f"Employee with ID {employee_id} not found.")
-        return
-
-    employee_name = user_response.json().get("name")
-
-    todos_response = requests.get(todos_url)
-    todos = todos_response.json()
-
-    total_tasks = len(todos)
-    done_tasks = [task for task in todos if task.get("completed")]
-    number_of_done_tasks = len(done_tasks)
-
-    print(
-        f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
-
-    for task in done_tasks:
-        print(f"\t {task.get('title')}")
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./todo.py <employee_id>")
-        sys.exit(1)
+    employee_Id = int(sys.argv[1])
 
-    try:
-        emp_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer.")
-        sys.exit(1)
-    fetch_employee_todo_progress(emp_id)
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    user_data_url = "https://jsonplaceholder.typicode.com/users"
 
+    user_response = requests.get(user_data_url)
+    todo_response = requests.get(todo_url)
+    # if todo_response.status_code & user_response.status_code == 200:
+    todos = todo_response.json()
+    users = user_response.json()
+    for user in users:
+        if user.get("id") == employee_Id:
+            employee_name = user.get("name")
+    # filter completed tasks
+    done = []
+    total = 0
+    completed = 0
+    for todo in todos:
+        if todo.get("userId") == employee_Id:
+            total += 1
+            if todo.get("completed"):
+                completed += 1
+                done.append(todo.get("title"))
+    # Display the progress information
+    print(f"Employee {employee_name} is done with tasks({completed}/{total}):")
+    for _ in done:
+        print(f"\t {_}")
